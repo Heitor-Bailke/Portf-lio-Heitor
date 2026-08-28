cd9689dee04bea87088201d6ae4a225b4a21f6e5
Heitor Bailke <heitorbailkedev@hotmail.com>
2026-08-28 13:41:09 -0300

    Corrige template do formulário EmailJS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   emailjs: {
     serviceId: 'service_curxf0g',
-    templateId: 'template_gp0orho',
+    templateId: 'template_5sbq4bd',
     publicKey: 'zR0BOigcNtxtLNeN9'
   }
 };
